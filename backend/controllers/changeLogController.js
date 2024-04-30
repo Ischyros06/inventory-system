14295e44d072be4a2072b2294e36cc70b5034bd3
Chester Evio <eviochester18@gmail.com>
2024-05-01 07:36:17 +0800

    timezone configure

--- backend/controllers/changeLogController.js
+++ backend/controllers/changeLogController.js
@@ -4,6 +4,20 @@ const renderChangeLog = async (req, res) => {
     try {
         const logs = await changeLog.find().sort({ createdAt: -1 });
 
+        // Convert createdAt date to GMT+08:00 (Asia timezone)
+        logs.forEach(log => {
+            log.createdAt = new Date(log.createdAt).toLocaleString('en-US', {
+                timeZone: 'Asia/Singapore', // Change the timezone to your desired Asia timezone
+                hour12: false, // Use 12-hour format
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            });
+        });
+
         res.render("changeLogs", { changeLogs: logs });
     } catch (error) {
         console.error("Error fetching items:", error);
